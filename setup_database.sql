5121da6d951151815f2cba0b2d009289d062d1cc
AnasJafir <dr.anas.jafir@gmail.com>
2024-05-09 01:41:35 +0100

    Setting up database

--- setup_database.sql
+++ setup_database.sql
@@ -2,10 +2,10 @@
 CREATE DATABASE IF NOT EXISTS ThootTrack;
 
 -- Create user
-CREATE USER IF NOT EXISTS 'Anas'@'localhost' IDENTIFIED BY 'alx0723';
+CREATE USER IF NOT EXISTS 'username'@'localhost' IDENTIFIED BY 'password';
 
-GRANT ALL PRIVILEGES ON ThootTrack.* TO 'Anas'@'localhost';
+GRANT ALL PRIVILEGES ON ThootTrack.* TO 'username'@'localhost';
 
-GRANT SELECT ON performance_schema.* TO 'Anas'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'username'@'localhost';
 
 FLUSH PRIVILEGES;
