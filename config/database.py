b2f71c725df5d4b891b1f8bf7c367663c877c216
AnasJafir <dr.anas.jafir@gmail.com>
2024-05-15 17:37:46 +0100

    Addition of documentations to classes and methods

--- config/database.py
+++ config/database.py
@@ -4,5 +4,5 @@ from flask_sqlalchemy import SQLAlchemy
 
 class Config:
     """Database Configuration"""
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/ThootTrack'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://Anas:alx0723@localhost/ThootTrack'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
